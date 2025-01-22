bfba6d35fba11c85189140a99f99663d30cd5c8f
Harley Dave Chavez <127485543+HATDOGnamayICE@users.noreply.github.com>
2025-01-22 16:29:56 +0800

    commit huehuehue

--- ruiz_clinic/admin.py
+++ ruiz_clinic/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-<<<<<<< HEAD
 
 admin.site.register(Item_Category)
 admin.site.register(Item_Frame_Type)
 admin.site.register(Sales)
 admin.site.register(Payment_Duration)
 admin.site.register(Purchased_Item)
-=======
-admin.site.register(Item_Category)
-admin.site.register(Item_Frame_Type)
->>>>>>> 905efb23243bffcac98ebe20bcaa329f407f1ea3
